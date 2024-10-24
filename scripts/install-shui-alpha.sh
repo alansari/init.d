ddbfaf62d8a1d42d5ba618e8531177274dd87fe4
alansari <terminal.v@gmail.com>
2024-10-24 11:05:24 +0300

    update repo if exists

--- scripts/install-shui-alpha.sh
+++ scripts/install-shui-alpha.sh
@@ -10,12 +10,18 @@ source "${USER_HOME:?}/init.d/helpers/functions.sh"
 # Ensure this script is being executed as the default user
 exec_script_as_default_user
 
-
-# Clone the GitHub repository
-rm -rf /home/default/.shui
-git clone https://github.com/alansari/frontend.git -b dev-SHUI /home/default/.shui
-cd /home/default/.shui
-
+# Check if the directory exists
+if [ -d "/home/default/.shui" ]; then
+    # If it exists, update the repository
+    cd /home/default/.shui
+    git fetch origin
+    git checkout dev-SHUI
+    git pull origin dev-SHUI
+else
+    # If it doesn't exist, clone the repository
+    git clone https://github.com/alansari/frontend.git -b dev-SHUI /home/default/.shui
+    cd /home/default/.shui
+fi
 
 # Create a Python virtual environment and activate it
 python3 -m venv .venv && source .venv/bin/activate  
